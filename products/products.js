a4fb7910f40591bbf22a7255d33eabf7a3bd2de1
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-02-04 17:59:48 +0800

    component done

--- products/products.js
+++ products/products.js
@@ -1,7 +1,7 @@
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
 
-let productModal = ""
-let delProductModal = ""
+let productModal = ''
+let delProductModal = ''
 
 const app = createApp({
 	data() {
